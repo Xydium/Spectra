9387cae03f2540e48d4b9cd7397c6ab357054d19
Xydium <prodriver54@gmail.com>
2018-08-02 13:20:51 -0400

    Improved Command Annotating

--- Spectra/source/Program.cs
+++ Spectra/source/Program.cs
@@ -453,6 +453,8 @@ DRAW_ROTATING_HUE_BREATHING    11 rotatingHue:(0-255) frequency:(0-255) brightne
 
 		static void Main(string[] args)
 		{
+            var c = new SpectraController();
+            c.RunSpectraCommandLine();          
 			Console.WriteLine("------ Spectra Controller ------");
 			InitializeCommands();
 			while (!ShouldQuit)
--- Spectra/source/SpectraController.cs
+++ Spectra/source/SpectraController.cs
@@ -11,29 +11,49 @@ namespace Spectra
 	class SpectraController
 	{
         private Dictionary<String, MethodInfo> Commands;
+        private bool running;
 
-        [SpectraCommand("h", 0, 1, "h {Command Name: String?}")]
+        [SpectraCommand("h", 0, 1, "Help: h {Command Name: String?}")]
         public void Help(String[] args)
         {
+            Console.WriteLine("Spectra Help (? = Optional, ! = Required):\n");
             switch(args.Length)
             {
                 case 0:
-                    Array.ForEach(Commands.Values.ToArray(), (m) => Console.WriteLine(m.GetCustomAttribute<SpectraCommand>().usage));
+                    Array.ForEach(Commands.Values.ToArray(), (m) => Console.WriteLine(m.GetCustomAttribute<SpectraCommand>().usage + '\n'));
                     break;
                 case 1:
-                    Console.WriteLine(Commands[args[0]].GetCustomAttribute<SpectraCommand>().usage);
+                    Console.WriteLine(Commands[args[0]].GetCustomAttribute<SpectraCommand>().usage + '\n');
                     break;
                 default:
-                    Console.WriteLine("Invalid use of command 'h'.");
+                    Console.WriteLine("Invalid use of command 'h'.\n");
                     break;
             }
+        }
+
+        [SpectraCommand("q", 0, 0, "Quit: q")]
+        public void Quit(String[] args)
+        {
+            running = false;
+        }
+
+        [SpectraCommand("sp", 2, 3, 
+@"Spectrum: sp {Display Mode: Int!} {Arg2: Int!} {Arg3: Int?}
+    Valid Display Modes:
+        1 = Static Hue // Arg2[0-255] = Hue // Arg3[0-255] = Frosting
+        2 = Static Rainbow // Arg2[0-255] = Frosting
+        3 = Scrolling Rainbow // Arg2[0-255] = Scroll Rate // Arg 3[0-255] = Frosting")]
+        public void Spectrum(String[] args)
+        {
+            Console.WriteLine("This is where I'd put my Spectrum constroller, if I had one!");
         }
 
 		public void RunSpectraCommandLine()
 		{
 			Commands = ParseSpectraCommands();
+            running = true;
 
-			while(true)
+			while(running)
 			{
 				Console.Write(">> ");
 
@@ -41,7 +61,10 @@ namespace Spectra
 				var command = input[0];
 				var args = input.Skip(1).ToArray();
 
-				Commands[command].Invoke(this, new object[] { args });
+                if (Commands.ContainsKey(command))
+                    Commands[command].Invoke(this, new object[] { args });
+                else
+                    Console.WriteLine("Invalid command. Execute command 'h' to see valid commands.\n");
 
 				Thread.Sleep(50);
 			}
